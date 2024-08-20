58a7f55087d594dda2f4255adbe4bea59493bd11
andrei7avram <66694499+andrei7avram@users.noreply.github.com>
2024-08-20 20:30:04 +0300

    adadad

--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PressurePlate : MonoBehaviour
@@ -11,22 +13,69 @@ public class PressurePlate : MonoBehaviour
     private float maximumMass = 2f;
 
     [SerializeField]
-    private bool isActivated;
+    private bool isActivated = false;
+
+    [SerializeField]
+    private GameObject panel;
+    private TextMeshProUGUI text;
+
+    private void Start() {
+        panel = GameObject.FindGameObjectsWithTag("pressure")[0];
+        text = panel.GetComponentInChildren<TextMeshProUGUI>();
+        
+    }
 
     
     
     void OnCollisionEnter(Collision collision)
     {   
-        if(collision.gameObject.layer == LayerMask.NameToLayer("Pickable"))
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Pickable") && !isActivated)
         {   
             float objectMass = collision.gameObject.GetComponent<Rigidbody>().mass;
             if(objectMass >= requiredMass && objectMass <= maximumMass)
             {
-                Debug.Log("Pressure plate activated!");
-                isActivated = true;
                 
+                isActivated = true;
+                StartCoroutine(PressureFeedbackActive());
+                
+            }else if(objectMass > maximumMass)
+            {
+                
+                StartCoroutine(PressureFeedbackTooHeavy());
+            } else if (objectMass < requiredMass)
+            {
+                
+                StartCoroutine(PressureFeedbackTooLight());
             }
         }
+    }
+
+    IEnumerator PressureFeedbackActive()
+    {
+        
+        text.text = "Pressure plate activated!";
+        text.color = Color.green;
+        panel.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        panel.SetActive(false);
+    }
+
+    IEnumerator PressureFeedbackTooHeavy()
+    {
+        text.text = "Object is too heavy!";
+        text.color = Color.red;
+        panel.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        panel.SetActive(false);
+    }
+
+    IEnumerator PressureFeedbackTooLight()
+    {
+        text.text = "Object is too light!";
+        text.color = Color.red;
+        panel.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        panel.SetActive(false);
     }
 
     public bool GetIsActivated()
@@ -34,12 +83,4 @@ public class PressurePlate : MonoBehaviour
         return isActivated;
     }
 
-    void OnCollisionExit(Collision collision)
-    {
-        if(collision.gameObject.layer == LayerMask.NameToLayer("Pickable") && isActivated)
-        {
-            isActivated = false;
-            Debug.Log("Pressure plate deactivated!");
-        }
-    }
 }
